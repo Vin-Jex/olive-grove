17b5dfabca8f4349d0664a0c1f19114460dec5e2
eniola <eniolafaithful57@gmail.com>
2024-11-20 15:09:49 +0100

    fix: remove breaking chages

--- pages/students/assessments/index.tsx
+++ pages/students/assessments/index.tsx
@@ -6,29 +6,29 @@ import withAuth from "@/components/Molecules/WithAuth";
 
 const Assessments = () => {
   return (
-    <StudentWrapper title='Assessments' metaTitle='Olive Groove ~ Assessments'>
-      <div className='p-6 sm:p-8 md:p-12 space-y-5'>
+    <StudentWrapper title="Assessments" metaTitle="Olive Groove ~ Assessments">
+      <div className="p-6 sm:p-8 md:p-12 space-y-5">
         {/* Title */}
-        <div className='flex flex-col'>
-          <span className='text-base sm:text-lg font-medium text-dark font-roboto'>
+        <div className="flex flex-col">
+          <span className="text-base sm:text-lg font-medium text-dark font-roboto">
             Access your Assessments
           </span>
-          <span className='text-sm sm:text-md text-subtext font-roboto'>
+          <span className="text-sm sm:text-md text-subtext font-roboto">
             Manage, submit and access your assessments.
           </span>
         </div>
 
-        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 h-fit w-full gap-6 sm:gap-8 mt-8'>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 h-fit w-full gap-6 sm:gap-8 mt-8">
           {subjectData.map((subject, index) => (
             <SubjectCard
               key={index}
               assessments
+              toggleModal={() => {}}
               name={subject.name}
               role={subject.role}
               time={subject.time}
               topic={subject.topic}
               subject={subject.subject}
-              btnLink1={subject.btnLink1}
               btnLink2={`/students/assessments/${subject.subject?.toLocaleLowerCase()}`}
             />
           ))}
