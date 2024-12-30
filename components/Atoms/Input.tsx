ea6e4676455bd60a63d1beb66bf49eb71902c4df
Okereke Ifeanyi Vincent <vincentcode0@gmail.com>
2024-12-29 23:35:15 -0800

    Feat: Update input component to be more flexible

--- components/Atoms/Input.tsx
+++ components/Atoms/Input.tsx
@@ -1,42 +1,14 @@
-import { InfoOutlined } from '@mui/icons-material';
-import React, { InputHTMLAttributes, useState } from 'react';
-import { OverridableComponent } from '@mui/material/OverridableComponent';
-import { SvgIconTypeMap } from '@mui/material';
+import {
+  InfoOutlined,
+  VisibilityOffOutlined,
+  VisibilityOutlined,
+} from "@mui/icons-material";
+import React, { InputHTMLAttributes, useState } from "react";
+import { InputType } from "../utils/types";
 
-export type InputType =
-  | 'button'
-  | 'text'
-  | 'checkbox'
-  | 'color'
-  | 'date'
-  | 'datetime-local'
-  | 'email'
-  | 'file'
-  | 'hidden'
-  | 'image'
-  | 'month'
-  | 'number'
-  | 'password'
-  | 'radio'
-  | 'reset'
-  | 'search'
-  | 'submit'
-  | 'tel'
-  | 'time'
-  | 'week'
-  | 'url';
+
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   validationError?: string;
-  hideIcon?:
-    | (OverridableComponent<SvgIconTypeMap<{}, 'svg'>> & {
-        muiName: string;
-      })
-    | any;
-  showIcon?:
-    | (OverridableComponent<SvgIconTypeMap<{}, 'svg'>> & {
-        muiName: string;
-      })
-    | any;
   isValid?: boolean | undefined;
   handleInputChange?: React.ChangeEvent<HTMLInputElement>;
   inputWidth?: number | string;
@@ -51,77 +23,67 @@ const Input: React.FC<InputProps> = ({
   isValid, // Boolean
   handleInputChange, // Function
   inputWidth, // Input size
-  hideIcon: HideIcon, // Icon (HTML Element)
-  showIcon: ShowIcon, // Icon (HTML Element)
   status, // Accepts warning, error, success strings only
   type: Type, // Input Type
   inputSize,
   ...inputProps // Other properties
 }) => {
-  // const [isValid, setIsValid] = useState<boolean | undefined>(undefined);
   const [showInput, setShowInput] = useState<boolean | undefined>(false);
 
-  // const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   const inputValue = event.target.value;
-  //   // Add your validation logic here
-  //   // Example validation: Input should not be empty
-  //   setIsValid(inputValue.trim() !== "");
-  // };
-
-  const showInputField = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const showInputField = () => {
     setShowInput(!showInput);
   };
 
   const statusColor =
-    status === 'success'
-      ? '#06C270'
-      : status === 'warning'
-      ? '#FFCC00'
-      : status === 'error'
-      ? '#FF3B3B'
-      : '#1E1E1E60';
+    status === "success"
+      ? "#06C270"
+      : status === "warning"
+      ? "#FFCC00"
+      : status === "error"
+      ? "#FF3B3B"
+      : "#1E1E1E60";
   return (
     <div
       className='space-y-1 w-full'
       style={{
         width:
-          inputWidth && typeof inputWidth === 'string'
-            ? ''
-            : inputWidth && typeof inputWidth === 'number'
-            ? '' + 'px'
-            : '',
+          inputWidth && typeof inputWidth === "string"
+            ? ""
+            : inputWidth && typeof inputWidth === "number"
+            ? "" + "px"
+            : "",
       }}
     >
       <div className='flex relative items-center w-full'>
         <input
-          type={showInput ? '' : Type}
+          type={showInput ? "" : Type}
           {...inputProps}
           className={`${className}  px-3 relative py-2 placeholder:text-sm placeholder:mt-1 !border-gray-200 rounded-lg`}
           style={{
-            width: '100%',
+            width: "100%",
             border: `1.5px solid ${statusColor}`,
-            outline: 'none',
+            outline: "none",
           }}
         />
-        {ShowIcon && showInput ? (
-          <ShowIcon
-            className='absolute right-3 top-1/2 -translate-y-1/2 text-subtext/50 !text-[1.4rem]'
+        {Type === "password" && showInput ? (
+          <VisibilityOutlined
+            className='absolute right-3 top-1/2 -translate-y-1/2 text-subtext/50 !text-[1.2rem]'
             onClick={showInputField}
           />
-        ) : HideIcon && !showInput ? (
-          <HideIcon
-            className='absolute right-3 top-1/2 -translate-y-1/2 text-subtext/50 !text-[1.4rem]'
+        ) : Type === "password" && !showInput ? (
+          <VisibilityOffOutlined
+            className='absolute right-3 top-1/2 -translate-y-1/2 text-subtext/50 !text-[1.2rem]'
             onClick={showInputField}
           />
         ) : (
-          ''
+          ""
         )}
       </div>
       {validationError && isValid && (
         <div className='flex items-center space-x-1'>
           <span className='flex items-center'>
             <InfoOutlined
-              sx={{ fontSize: '1.2rem', color: `${statusColor}` }}
+              sx={{ fontSize: "1.2rem", color: `${statusColor}` }}
             />
           </span>
           <p style={{ color: `${statusColor}` }}>{validationError}</p>
