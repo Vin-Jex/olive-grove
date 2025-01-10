f7aff7658da65b5ef72902c830600a3ae28201c1
Okereke Ifeanyi Vincent <vincentcode0@gmail.com>
2025-01-10 11:06:59 -0800

    Feat: Update Tab Component

--- components/Molecules/Tab/Tab.tsx
+++ components/Molecules/Tab/Tab.tsx
@@ -79,27 +79,25 @@ const Tab: FC<{
   // }, [viewedSegment]);
 
   return (
-    <div className="w-full flex flex-col gap-6">
+    <div className='w-full flex flex-col gap-6'>
       {/* TAB ACTIONS */}
-      <div className="w-full flex gap-0">
+      <div className='w-full flex gap-0'>
         {slugs.map((slug, i) => (
-          <>
-            <div
-              className={`px-7 py-2 font-medium text-sm cursor-pointer transition ${
-                activeTab === slug.key
-                  ? "border-primary border-opacity-70 border-b-2 bg-[#32A8C41A] text-primary"
-                  : ""
-              }`}
-              onClick={() => setActiveTab(slug.key)}
-              key={i}
-            >
-              {capitalize(slug.name)}
-            </div>
-          </>
+          <div
+            className={`px-7 py-2 font-medium text-sm cursor-pointer transition ${
+              activeTab === slug.key
+                ? "border-primary border-opacity-70 border-b-2 bg-[#32A8C41A] text-primary"
+                : ""
+            }`}
+            onClick={() => setActiveTab(slug.key)}
+            key={i}
+          >
+            {capitalize(slug.name)}
+          </div>
         ))}
       </div>
       {/* TAB BODY */}
-      <div className="w-full">
+      <div className='w-full'>
         {body.find((content) => content.slug === activeTab)?.content}
       </div>
     </div>
